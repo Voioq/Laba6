ed3d8e5afc3d57ffb54b3bfb17d496998ac4cd5c
Vladimir <volodya_kazaryan@mail.ru>
2022-04-21 11:48:04 +0500

    Добавление функции delete_index delete_index - удаляет строку по указанному индексу

--- py/main.py
+++ py/main.py
@@ -2,10 +2,19 @@ def insert_mid(list, line):
     i = len(list) // 2
     list.insert(i,line)
     print(list)
-    
+
+
 def insert_index(list, line, number):
     if len(list) < number:
         print("Operation is not possible")
     else:
         list.insert(number,line)
+        print(list)
+
+
+def delete_index(list, number):
+    if len(list) <=  number:
+        print("Delete operation is not possible")
+    else:
+        list.pop(number)
         print(list)
